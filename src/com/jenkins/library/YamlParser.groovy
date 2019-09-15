02958f5d1ff0fd5da8deab9af590f78937d541f1
test <shivendra.sharma@sapient.com>
2019-09-15 09:02:30 +0100

    testing

---
+++ src/com/jenkins/library/YamlParser.groovy
@@ -0,0 +1,16 @@
+package com.jenkins.library
+
+
+def YamlParser(Map config) {
+    Map yamlConfig = [:]
+
+     yamlConfig.configPath = config.configPath ? config.path : "${pwd()}"
+     yamlConfig.fileName = config.fileName ? config.fileName : "values.yaml"
+     yamlConfig.keyName = config.keyName
+     yamlConfig.keyValue = config.keyValue
+
+    def tempfile = yamlConfig.configPath + "temp-" + UUID.randomUUID().toString() + ".txt"
+    def targetfile = yamlConfig.configPath + yamlConfig.fileName
+    sh "yq w ${targetfile} ${yamlConfig.keyName} ${yamlConfig.keyValue} >${tempfile}"
+    sh "cp -r ${tempfile} ${targetfile} && rm -rf ${tempfile}"
+}
--- vars/yamlReplace.groovy
+++ vars/yamlReplace.groovy
@@ -2,16 +2,9 @@
 //import com.jenkins.library.YamlParser
 
 def call(Map config=[:]) {
-    Map yamlConfig = [:]
-     yamlConfig.configPath = config.configPath ? config.path : "${pwd()}"
-     yamlConfig.fileName = config.fileName ? config.fileName : "values.yaml"
-     yamlConfig.keyName = config.keyName
-     yamlConfig.keyValue = config.keyValue
 
-    //def yaml = new YamlParser();
-    //yaml.parser(yamlConfig);
-
-    println('!!! ' + yamlConfig)
+    def yaml = new YamlParser();
+    yaml.parser(config);
 
     return true
 }
